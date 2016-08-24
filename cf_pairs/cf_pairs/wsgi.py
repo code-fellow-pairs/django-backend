3b7e29d1539043a854912e6686179e005dae68dd
muniri92 <mibrah04@gmail.com>
2016-08-24 07:53:36 -0700

    basic django scaffold, models next

---
+++ cf_pairs/cf_pairs/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cf_pairs project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cf_pairs.settings")
+
+application = get_wsgi_application()
---
+++ cf_pairs/cf_pairs_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CfPairsAppConfig(AppConfig):
+    name = 'cf_pairs_app'
